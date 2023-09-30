caec50bb33f8b3635a0ac025a5fb1ed624b22e87
Gw3i <contact.johngoldco@gmail.com>
2023-09-30 15:46:48 +0200

    Install dependencies and setup web socket connection

---
+++ src/app/api/ticker/route.ts
@@ -0,0 +1,40 @@
+import { Ticker } from '@/types/coins/ticker.types';
+import { NextResponse } from 'next/server';
+
+export function GET(response: NextResponse) {
+  try {
+    const { Console } = require('console');
+    const { WebsocketStream } = require('@binance/connector');
+
+    const logger = new Console({ stdout: process.stdout, stderr: process.stderr });
+
+    const callbacks = {
+      open: () => logger.debug('Connected with Websocket server'),
+      close: () => logger.debug('Disconnected with Websocket server'),
+      message: (data: Ticker) => logger.info(data),
+    };
+
+    const websocketStreamClient = new WebsocketStream({ logger, callbacks });
+
+    // all pairs
+    // websocketStreamClient.ticker()
+
+    // single pair
+    const coin: Ticker = websocketStreamClient.ticker('bnbusdt');
+
+    setTimeout(() => {
+      websocketStreamClient.ticker('btcbusd');
+    }, 3000);
+
+    // ping server
+    setTimeout(() => websocketStreamClient.pingServer(), 4000);
+
+    setTimeout(() => websocketStreamClient.disconnect(), 6000);
+
+    return new Response(JSON.stringify(coin));
+  } catch (error) {
+    console.error(error);
+    // Handle errors and return an error response
+    return new Response('Could not connect to Ticker. Please try again or contact support', { status: 500 });
+  }
+}
