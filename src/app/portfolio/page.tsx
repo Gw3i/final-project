989b034abec0b98a809fb9e3756d1b4a421b9619
Gw3i <contact.johngoldco@gmail.com>
2023-11-22 23:31:19 +0100

    Add Accordion component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,14 +1,24 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { FC } from 'react';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <section>
-      <section className="grid max-w-xl mx-auto mt-8">
-        <h2 className="text-headline-small mb-4">Portfolio</h2>
-        <p className="text-zinc-500 text-sm mb-8">See all you assets in one place</p>
-      </section>
+    <section className="grid max-w-xl mx-auto mt-8">
+      <h2 className="text-headline-small mb-4">Portfolio</h2>
+      <p className="text-zinc-500 text-sm mb-8">See all you assets in one place</p>
+
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Binance</AccordionTrigger>
+          <AccordionContent>Binance assets table</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Kraken</AccordionTrigger>
+          <AccordionContent>Empty for now</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   );
 };
