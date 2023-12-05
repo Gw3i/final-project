f8fc6f1b4361d1f81147d803f89fcfc4d7713fd0
Gw3i <contact.johngoldco@gmail.com>
2023-12-05 02:05:06 +0100

    Add skeleton to AssetCard when assets are loading

--- src/components/AssetCard.tsx
+++ src/components/AssetCard.tsx
@@ -3,6 +3,7 @@ import { NormalizedBalanceWithCurrentPrice } from '@/types/user-data/balance.typ
 import { KrakenBalanceWithCurrentPrice } from '@/types/user-data/kraken-user-data.types';
 import { FC } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/Table';
+import { Skeleton } from './ui/skeleton';
 
 interface AssetCardProps {
   exchangeName: string;
@@ -22,22 +23,43 @@ const AssetCard: FC<AssetCardProps> = ({ exchangeName, assets, isLoading }) => {
     <article className="bg-slate-100 rounded-lg py-6 px-4">
       <div className="flex justify-between items-center uppercase font-semibold mb-4 text-2xl">
         <p>{exchangeName}</p>
-        <p>${total.toFixed(2)}</p>
+        <div>
+          {isLoading ? <Skeleton className="bg-zinc-500 w-[100px] h-[30px] rounded-md" /> : <p>${total.toFixed(2)}</p>}
+        </div>
       </div>
 
-      {isLoading && <p>LOADING...</p>}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Asset</TableHead>
+            <TableHead className="text-right">Price</TableHead>
+            <TableHead className="text-right">Balance</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isLoading &&
+            [1, 2, 3, 4, 5].map((_, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium ">
+                  <Skeleton className="bg-zinc-500 w-[40px] h-[20px] rounded-md" />
+                </TableCell>
+                <TableCell className="">
+                  <div className="flex justify-end">
+                    <Skeleton className="bg-zinc-500 w-[100px] h-[20px] rounded-md" />
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="grid justify-items-end gap-1">
+                    <Skeleton className="bg-zinc-500 w-[60px] h-[18px] rounded-md" />
+                    <Skeleton className="bg-zinc-500 w-[120px] h-[18px] rounded-md" />
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
 
-      {assetList.length > 1 && !isLoading && (
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Asset</TableHead>
-              <TableHead className="text-right">Price</TableHead>
-              <TableHead className="text-right">Balance</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {assetList.map(
+          {assetList.length > 0 &&
+            !isLoading &&
+            assetList.map(
               (asset) =>
                 !asset.isStaked && (
                   <TableRow key={asset.name}>
@@ -56,9 +78,8 @@ const AssetCard: FC<AssetCardProps> = ({ exchangeName, assets, isLoading }) => {
                   </TableRow>
                 ),
             )}
-          </TableBody>
-        </Table>
-      )}
+        </TableBody>
+      </Table>
     </article>
   );
 };
